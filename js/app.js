6c527232a9d46165964fc0fad37fa52e7b622bc9
James <james@jjgportfolio.com>
2014-08-23 18:14:31 +0100

    Karma installation test

---
+++ js/app.js
@@ -0,0 +1,8 @@
+/**
+ * Created by jgpro_000 on 23/08/14.
+ */
+describe("A suite", function() {
+    it("contains spec with an expectation", function() {
+        expect(true).toBe(true);
+    });
+});
